2c79c4cff260de5213a3bebdf33afa7c7af0ab7e
logyshka <kfirddsa@gmail.com>
2023-10-10 17:40:09 +0300

    OwnerExistsFilter + router(Command("owner"))

--- src/common/filters/owner_exists.py
+++ src/common/filters/owner_exists.py
@@ -1,8 +1,9 @@
 from aiogram.filters import Filter
+from aiogram.types import Message
 
 from src.data.config import Config
 
 
 class OwnerExistsFilter(Filter):
-    async def __call__(self, config: Config):
+    async def __call__(self, msg: Message, config: Config):
         return config.main_app.owner_id == 0
