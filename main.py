55308cd71ee3225fe3d1cffa85507d8701c5a33e
logyshka <75390848+logyshka@users.noreply.github.com>
2023-10-10 17:03:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import logging
+
+import coloredlogs
+
+from src.apps import (root, )
+from src.data.config import parse_config
+from src.data.const import *
+from src.database import init_database
+
+
+async def main():
+    coloredlogs.install(level=logging.INFO)
+    config = parse_config(config_file=CONFIG_FILE)
+
+    await init_database(config)
+
+    # Запуск главного приложения.
+    await root.app.start(
+        bot_token=config.main_app.bot_token,
+        context={
+            "config": config
+        },
+    )
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except (KeyboardInterrupt, SystemExit):
+        logging.warning("Bot stopped!")
